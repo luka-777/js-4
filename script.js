f0d42f7a314db713bb68084d769f4f37a098d844
luka-777 <lukagiunashvili2@gmail.com>
2024-01-16 15:46:26 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,79 @@
+
+// // 1)
+// function pensioner(myAge, pensionAge){
+//     let finishAge = pensionAge - myAge;
+//     return finishAge
+// }
+
+// console.log(pensioner(20,65));
+
+
+
+
+
+
+
+// // 2)
+// function calculatore(num1,sign,num2) {
+//     if(num1 === "" || num2 === "" || sign === ""){
+//         console.log("გთხოვთ ჩაწეროთ რამე")
+//     }else if(sign === "+"){
+//         console.log(num1 + num2)  
+//     }else if(sign === "-"){
+//         console.log(num1 - num2)
+//     }else if(sign === "*"){
+//         console.log(num1 * num2)
+//     }else if(sign === "/"){
+//         console.log(num1 / num2)
+//     }else{
+//         console.log("error")
+//     }
+// }
+
+
+
+
+
+
+// // 3)
+// function checkPAsswordStrenght(password) {
+//     if (password.length < 8){
+//     console.log("დასარეგისტრირებლად გთხოვთ შეიყვანოთ მინიმუმ 8 სიმბოლო");
+//    }else if(password.length > 16){
+//     console.log("დასარეგისტრირებლად გთხოვთ შეიყვანოთ მაქსიმუმ 16 სიმბოლო");
+//    }else{
+//     console.log("თქვენ წარმატებით დარეგისტრირდით")
+//    }
+// }
+
+//checkPAsswordStrenght('');
+
+
+
+
+
+
+
+// // 4)
+// function flowerType (flower){
+//     switch (flower) {
+//         case "ნარცისი":
+//             console.log("ნაპოვნია")
+//             break;
+//             case "ტიტა":
+//             console.log("ნაპოვნია")
+//             break;
+//             case "იასამანი":
+//             console.log("ნაპოვნია")
+//             break;
+    
+//         default:
+//             console.log("არ არის ნაპოვნი")
+//             break;
+//     }
+  
+// }
+
+// flowerType('იასამანი');
+
+
